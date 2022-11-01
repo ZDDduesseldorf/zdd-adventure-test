93c5127db6a2a8e12336536253b1fe8dead9b5db
florian-huber <florian.huber@hs-duesseldorf.de>
2022-11-01 12:37:10 +0100

    create start files

---
+++ run.py
@@ -0,0 +1,3 @@
+from zdd_adventure.start_adventure import start
+
+start()
---
+++ zdd_adventure/start_adventure.py
@@ -0,0 +1,8 @@
+"""This starts the adventure
+"""
+def start():
+    for _ in range(5):
+        print(20 * ">")
+    print(">>>> Something awesome is about to happen...")
+    for _ in range(5):
+        print(20 * ">")
