b3e83ed8b5520d733ddcaea9c2168b4c86d265d0
bro-tec <khan0shazil@gmail.com>
2022-11-09 14:38:43 +0100

    save und json korrigiert

--- zdd_adventure/save.py
+++ zdd_adventure/save.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 
 import json
-  
+
 
 def load_data():
     # Opening JSON file
     f = open('player_data.json')
-      
-    # returns JSON object as 
+
+    # returns JSON object as
     # a dictionary
     data = json.load(f)
     f.close()
-    
-def save_data():
-    pass
+    return data
+
+def save_data(data):
+    with open('data.json', 'w') as f:
+        json.dump(data, f)
