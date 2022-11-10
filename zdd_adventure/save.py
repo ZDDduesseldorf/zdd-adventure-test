b52c97f9f4f29066f52c10dd11305abe530c3f51
bro-tec <bro@bro.fire>
2022-11-10 21:09:33 +0100

    added save player stats and create or use player

--- zdd_adventure/save.py
+++ zdd_adventure/save.py
@@ -4,25 +4,43 @@ import json
 
 def load_data():
     # Opening JSON file
-    f = open('player_data.json', "r")
+    f = open('zdd_adventure\player_data.json', "r")
 
     # returns JSON object as a dictionary
     data = json.load(f)
     f.close()
     return data
 
+def load_player_data(name):
+    # Opening JSON file
+    f = open('zdd_adventure\player_data.json', "r")
+
+    # returns JSON object as a dictionary
+    data = json.load(f)
+    f.close()
+    data2 = data[name]
+    return data2
+
 def save_data(data):
-    with open('player_data.json', 'w') as f:
-        json.dump(data, f)
+    ld = load_data()
+    ld[data["name"]] = data
+    with open('zdd_adventure\player_data.json', 'w') as f:
+        json.dump(ld, f)
         
-def game_over():
+def create_new_player(name):
     data = {}
-    data["name"] = ""
+    data["name"] = name
+    game_over(data)
+
+def game_over(data1):
+    name = data1["name"]
+    data = {}
+    data["name"] = name
     data["inventory"] = [
         "Studierendenausweis"
     ]
     data["floor"] = 1
     data["room"] = 1
     data["visited_rooms"] = []
-    with open('player_data.json', 'w') as f:
-        json.dump(data, f)
+    save_data(data)
+    return data
--- zdd_adventure/start_adventure.py
+++ zdd_adventure/start_adventure.py
@@ -13,11 +13,32 @@ def start():
         print(20 * ">")
     print(">>>> Loading Data...")
     #spieldaten laden
-    data = save.load_data()
-    print(data)
-    floor = {1: room_basement_1.room_basement_1(data)}
+    acc = save.load_data()
+    accounts = list(acc.keys())
+    my_game = ""
+    while my_game == "":
+        print("\n\nChoose your Game-stats\n\n0: New Game")
+        for i in range(len(accounts)):
+            print((i+1), accounts[i])
+        my_game = input("\nInsert your Game:")
+        if my_game.isnumeric():
+            if not int(my_game) <= len(accounts):
+                my_game = ""
+        else:
+            my_game = ""
+    name = ""
+    if my_game == "0":
+        print("Welcome newcommer! \nFirst tell me your Name") # falls das in dritte form geschrieben werden soll bitte korrigieren
+        while (name == "") or (name in accounts):
+            name = input("Enter your name: ")
+        save.create_new_player(name)
+    else:
+        name = accounts[(int(my_game)-1)]
+        print('Welcome back{name}!')
+    data = save.load_player_data(name)
+    rooms = {1: {1: room_basement_1.room_basement_1(data)}}
     while not end_game:
-        floor[data["floor"]]
+        rooms[data["floor"]][data["room"]]
     
     
 
