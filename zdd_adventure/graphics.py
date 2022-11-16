997347df00b59141f8dc9f5bffc008df4c128257
l4nz8 <vin.bohm456@gmail.com>
2022-11-16 15:42:09 +0100

    2 new functions (print_ascii and clear cmd)

--- zdd_adventure/graphics.py
+++ zdd_adventure/graphics.py
@@ -1,12 +1,7 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov  9 15:49:08 2022
-
-@author: tillteb
-"""
 import time
 import sys
+from os import system, name
+import os
 import datetime
 
 # grafical funktion for printing delayed text
@@ -16,7 +11,7 @@ def delayed_print_output(pattern):
         for x in range(len(pattern)):
             print(pattern[x], end= '')
             sys.stdout.flush()
-            DELAY_TIME = time.sleep(0.005)
+            DELAY_TIME = time.sleep(0.5)
             DELAY_TIME
     return slower_text(pattern)
 
@@ -28,3 +23,21 @@ def countdown(time_sec):
         sys.stdout.flush()
         time_sec -= 1
         time.sleep(1)
+
+def clear_cmd():
+ 
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+ 
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+        
+def ascii_output(picture):
+    root = os.getcwd()
+    filename = os.path.join(root, "ASCII_art", picture)
+    ascii_data = open(filename,"r")
+    content = ascii_data.read()
+    ascii_data.close()
+    print(content)
