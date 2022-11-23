ba780efb979dc82a6d4e5cca8a51bc7235cb0c33
till-teb <till.beruflich@hotmail.com>
2022-11-23 01:41:05 +0100

    new version of lecture hall

--- zdd_adventure/room_basement_1.py
+++ zdd_adventure/room_basement_1.py
@@ -1,240 +1,299 @@
-<<<<<<< HEAD
-from zdd_adventure.graphics import delayed_print_output
-from zdd_adventure.graphics import countdown
-import room_2_liza
-=======
-from zdd_adventure.graphics import slow_print
+from zdd_adventure.closed_room_first_floor import slow_list_print
+from zdd_adventure.graphics import slow_print, countdown, ascii_output
+from zdd_adventure.InputChecker_and_DrunkMode import player_input
 
+whiteboard = ['T was here']
 
->>>>>>> main
-
-
-# 3 funktionality's in lecture hall: ( room_basement_1)
-
-# 1.search for the light switch
-# 2.look for more evidence
-# 3.leave the room
-# ['''
-# Slowly {game.name} stands up. Since the room is so extremely dark, it's very clever to try to turn on the light.
-# As always, the light switch is near the door. {game.name} still needs much longer than usual for the short way.
-# Completely understandable, considering its condition.
-# {game.name} stands with wobbly legs in front of the light switch and presses the switch.
-
-# ......[click]...........
-
-
-# {game.name} is lucky.
-# It seems like the power is working without a band, as the room is now shining in a rather bright light.
-
-# What does {game.name} want to do? '''
-
-# text_pattern_3 = f'''
-# At first {game.name} looks around slowly.
-# {game.name} get´s an idea. Maybe there are more clues on the desk....
-# So {game.name} starts walking slowly to the teachers desk.
-# When {game.name} arrives at the desk, he finds an almost empty desk. There are only the following items on the desk.
-
-#     -> A water bottle that has been started
-#     -> A cross screwdriver
-#     -> A pack of colored board markers
-
-#     However, {game.name} only has room for one item.
-# '''
-
-# text_pattern_1 = f'''
-# {game.name} open their eyes very slow. All {game.name} can see is a blurred dark room.
-# After a certain amount of time {game.name} is able to find out where he is currently located.
-# {game.name} is in the middle of his old lecture hall and {game.name} is really confused. {game.name} no longer understands
-# the world.
-# Why should {game.name} wake up in the old lecture hall of his old university?
-# For {game.name} this all makes no sense.
-# No matter how hard their tries to remember, {game.name} doesn't remember anything at all.
-
-# {game.name} is just trying to keep calm and think about what
-# options he has to find out what happened...
-
-
-
-#    game.visited_rooms += ['lecture hall']
-
-
+# WHITEBOARD ANPASSEN
+ascii_output("aktion_open_door.txt", True, 0.0003)
 def lecture_hall(game):
-    delayed_print_output(f'{game.name} open their eyes very slow.'+2*'\n')
+    text_pattern = ['''
+██      ███████  ██████ ████████ ██    ██ ██████  ███████ 
+██      ██      ██         ██    ██    ██ ██   ██ ██      
+██      █████   ██         ██    ██    ██ ██████  █████   
+██      ██      ██         ██    ██    ██ ██   ██ ██      
+███████ ███████  ██████    ██     ██████  ██   ██ ███████
+                    ''','''
+██   ██  █████  ██      ██      
+██   ██ ██   ██ ██      ██      
+███████ ███████ ██      ██      
+██   ██ ██   ██ ██      ██      
+██   ██ ██   ██ ███████ ███████ 
+                    ''']
+    slow_list_print(text_pattern, delay=1)
+    slow_print(f'{game.name} open their eyes very slow.'+2*'\n')
     countdown(4) 
     print( '\n')
-    delayed_print_output(f' All {game.name} can see is a blurred dark room.'+2*
+    slow_print(f'All {game.name} can see is a blurred dark room.'+2*
                          '\n'
                          +f'After a certain amount of time {game.name} is able to find out where he is currently located.'
                          +'\n')
     enter = input('### press enter ### \n')
     enter
-    delayed_print_output(f'{game.name} is in the middle of his old lecture hall.' +'\n'
-                         + f'{game.name} no longer understands the world.')
-    delayed_print_output(f''' Just trying to keep calm and think about what
-                         options {game.name} has to find out what happened...''')
+    slow_print(f'{game.name} is in the middle of their old lecture hall.' +'\n'
+                         + f'{game.name} no longer understands the world... \n')
+    slow_print(f'''Just trying to keep calm and think about what
+options {game.name} has to find out what happened...''')
     enter
-    choise = input(delayed_print_output('''choose option:
-          
-          --> 1. go to teachers desk
-          
-          --> 2. go to student stair rows
-          
-          --> 3. leave the room
-          '''))
+    choices = ['go to teachers desk','go to student chair rows', 'leave the room']
+    print( '\n')
+    choise = player_input(choices, decision_beer=0)
+    
     enter
-    if choise == '1':
-        pass
-    if choise == '2':
-        stair_rows()
-    if choise == '3':
-        pass
+    if choise == 1:
+        teachers_desk(whiteboard)
+    if choise == 2:
+        chair_rows()
+    if choise == 3:
+        leave_room()
     
+def leave_room():
+    '''
+    two ways possible to leave the lecture hall. 
+    Directly by exit-door or by exit-door but before visiting the storage room.
+    '''
+    def storage_room():
+        '''
+        Side door room bevor leaving the lecture hall.
+        To open the door its nessesarry to find the storage room key by the chair rows.
+        '''
+        slow_print(f'{game.name} slowly goes to the storage room door ...')
+        countdown(3)
+        print('\n')
+        
+        # Query if storage room key is in invenrory
+        
+        if 'storage room key' in game.inventory:
+            slow_print('''The storage room is open now. The room is very small. 
+Unfortunately nothing to find here besides a disinfectant bottle''')
+            game.inventory += ['disinfectant bottle']
+            enter =  input('### press enter ### \n')
+            slow_print(f'{game.name} is now leaving the lecture hall with his disinfectant bottle...')
+            countdown(5)
+            game.visited_rooms += ['lecture hall']
+            enter 
+        else:
+            slow_print(f'ohh noo, {game.name} need the storage room key to open the door.')
+            enter = input('### press enter ### \n')
+            enter
+            slow_print(f'{game.name} is now leaving the lecture hall...')
+            countdown(5)
+            game.visited_rooms += ['lecture hall']
+            enter
     
-def stair_rows():
-    choise = input(delayed_print_output(f'''
-                   
-            {game.name} is standing besides the student stair rows.
+    # leaving the lecture hall
+    print('\n')
+    slow_print(f'Now {game.name} will leave the lecture hall')
+    enter =  [input('### press enter ### \n')]
+    countdown(3)
+    print('\n')
+    slow_print(f'''ohhh wait, whats this? Another door? 
+{game.name} found a storage room door.  \n ''')
+    slow_print(f'For leaving the room which door should {game.name} choose?')
+    choises = ['storage room door','exit door']
+    # 2 possible ways for leaving
+    choise = player_input(choises, decision_beer=0)
+    if choise == 1:
+        storage_room()
+    if choise == 2:
+        slow_print(f'{game.name} is now leaving the lecture hall...')
+        countdown(5)
+        enter
+
+
+def teachers_desk(whiteboard):
+    '''
+    '''
+    
+    def look_at_whiteboard(whiteboard):
+        ### ASCI Art Pic missing
+        countdown(3)
+        print('\n')
+        print(whiteboard)
+        print('\n')
+        
+    def write_on_whiteboard(whiteboard):
+        if 'Whiteboard marker' in game.inventory:
+            entry = str(input(f'What does {game.name} want to add on the  whiteboard \n'))
+            whiteboard = whiteboard.append(entry)
+            print('\n gespeichert')
+            return whiteboard
+        
+        else:
+            slow_print(f'Ohhh noo, {game.name} missed to pick up the boardmarker.')
             
-            What does {game.name} want to do now?
+    
+########################################## teachers desk
+
+    slow_print(f'Now {game.name} search the teachers desk for more informations.')
+    print('\n')
+    countdown(6)
+    print('\n')
+    slow_print(f'''{game.name} has found three things on the teeachers desk. 
+{game.name} only can pick up ONE Item''')                                                 #grammatik ???????
+    enter = input('### press enter ### \n')    
+    slow_print('''--> Whiteboard marker
+               
+--> The DAISY degree documentaion
+
+--> A party flyer from the Media Faculty
+    ''')
+    slow_print(f'What does {game.name} want to pick up? \n')
+    choises = ['Whiteboard marker','The DAISY degree documentaion','A party flyer from the Media Faculty']
+    choise = player_input(choises,decision_beer=0)
+    if choise == 1:
+        slow_print('Whiteboard marker picked up.')
+        game.inventory += ['Whiteboard marker']
+    if choise == 2:
+        slow_print('DAISY degree documentaion picked up.')
+        game.inventory += ['The DAISY degree documentaion']
+    if choise == 3:
+        slow_print('Party flyer from the Media Faculty picked up.')
+        game.inventory += ['A party flyer from the Media Faculty']
+        
+########################################## whiteboard
+    print('\n')
+    enter
+    print('\n')
+    slow_print(f'''Because {game.name} is so close to the whiteboard right know {game.name} recognize 
+that something is written there.
+               ''')
+    print('\n')
+    enter =  [input('### press enter ### \n')]
+    ready_to_leave = True
+    while ready_to_leave == True:
+        enter
+        slow_print(f'What does {game.name} want to do?')
+        choises = ['look at the whiteboard', 'write something on the board','leave the room']
+        choise = player_input(choises, decision_beer=0)
+        # WHITEBOARD ALS BILD EINGEBEN
+        whiteboard = ['Hello there. XD was here. Just for your information ']
+        
+
+        if choise == 1:
+            look_at_whiteboard(whiteboard)
+        if choise == 2:
+            write_on_whiteboard(whiteboard)
+            print('Whiteboard: \n' )
+            for i,y in enumerate(whiteboard):
+                print(whiteboard[i], y) 
+                
+        if choise == 3:
+            return ready_to_leave == True
             
-            --> 1. sit down and relax for some time
-       
-            --> 2. search stair row 1
-                   
-            '''))
-    if choise == '1':
-        pass
-    if choise == '2':
-        pass
-    
-<<<<<<< HEAD
+        leave_room()
     
 
-=======
-    Der Raum ist jetzt beleutet.
-    Was möchtest du tun? '''
-    slow_print(text_pattern_2)
-    player_choice = int(input('''
-    1. Umsehen nach Anhaltszeichen
-    2. Raum verlassen
-    '''))
-    #game.make_choice(choices)
-
-    if player_choice == 1:
-        find_evidence(data)
-
-    if player_choice == 2:
-        leave_room(data)
-    else:
-        "Deine Antwort muss 1 oder 2 lauten."
-        player_choice
-              
-def find_evidence(data):
-    name = data["name"]
-    player_choice = 0
-    text_pattern_3 = f'''
-    {name} beginnt sich langsam Richtung Vorlesungspult zu begeben. Vielleicht liegen dort Hinweise...
-    Als {name} am Pult ankommt, liegen folgende Items auf dem Pult.
     
-    -> Eine angefangene Wasserflasche
-    -> Ein Schraubenzieher
-    -> Ein Packet Boardmarker
-    
-    {name} hat jedoch nur Platz für ein Item. 
-    '''
-    slow_print(text_pattern_3)
-    player_choice = int(input(f'''
-    1. Wasserflasche
-    2. Schraubenzieher
-    3. Boardmarker
-    
-    Welches Item möchte {name} aufheben?
-    '''))
-    if player_choice == 1:
-        print('Wasserflasche aufgehoben')
-        data["inventory"] += ['Wasserflasche']
-        leave_room(data)
         
-    if player_choice == 2:
-        print('Schraubenzieher aufgehoben')
-        data["inventory"] += ['Schraubenzieher']
-        leave_room(data)
-        
-    if player_choice == 3:
-        print('Boardmarker aufgehoben')
-        data["inventory"] += ['Boardmarker']
-        leave_room(data)
-    else:
-        "Deine Antwort muss 1, 2 oder 3 lauten."
-        player_choice
-        
-def leave_room(data):
-    name = data["name"]
-    txt = f'''
-    {name} hat den Vorlesungssaal jetzt wieder verlassen und steht jetzt auf dem Flur. 
-    {name} steht nur da und fragt sich, wie es weiter gehen soll. 
+def chair_rows():
     '''
-    slow_print(txt)
-
-
-
-
-
-
-
-
-
-
-# main funktion for LECTURE HALL
-def room_basement_1(data):
-    name = data["name"]
-    print(name)
-    ''' wake up in the lecture hall '''
-    text_pattern_1 = f''' 
-    {name} macht langsam die Augen auf. Sie lassen sich nur sehr langsam öffnen...
-    Langsam aber sicher erkennt {name} die Umgebung wieder. Es ist die Hochschule. 
-    Niemand weiß, wie {name} dort hingekommen war, doch jetzt ist {name} hier...
-    Aber was soll {name} jetzt machen? Der Kopf ist völlig leer:
-    ?...
-    
     '''
+    def chair_row_1():
+        slow_print(f'{game.name} is wakling through all seats from the first chair row.') 
+        print('\n')
+        countdown(6)
+        print('\n')
+        slow_print('Unfortunately nothing was found.')
+        print('\n')
+        slow_print(f'Now {game.name} has to choose again:')
+        print('\n')
+        choises_3 = ['search chair row 2', 'go back']
+        choise = player_input(choises_3, decision_beer=0)
+        if choise == 1:
+            chair_row_2()
+        if choise == 2:
+            go_back()
+            
+    def chair_row_2():
+        slow_print(f'{game.name} is wakling through all seats from the second chair row.') 
+        print('\n')
+        countdown(6)
+        print('\n')
+        slow_print(f'{game.name} finds a book with the title "gradient descent"...')
+        countdown(3)
+        game.inventory += ['book "gradient descent" ']
+        print('\n')
+        slow_print('Item: book "gradient descent" picked up')
+        print('\n')
+        slow_print(f'Now {game.name} has to choose again:')
+        print('\n')
+        choises_3 = ['search chair row 3', 'go back']
+        choise = player_input(choises_3, decision_beer=0)
+        if choise == 1:
+            chair_row_3()
+        if choise == 2:
+            go_back()
+        
+    def chair_row_3():
+        slow_print(f'{game.name} is wakling through all seats from the third chair row.') 
+        print('\n')
+        countdown(6)
+        print('\n')
+        slow_print('Unfortunately nothing was found.')
+        print('\n')
+        slow_print(f'Now {game.name} has to choose again:')
+        print('\n')
+        choises_3 = ['search chair row 4', 'go back']
+        choise = player_input(choises_3, decision_beer=0)
+        if choise == 1:
+            chair_row_4()
+        if choise == 2:
+            go_back()
+            
+    def chair_row_4():
+        slow_print(f'{game.name} is wakling through all seats from the fourth chair row... ')
+        print('\n')
+        countdown(6)
+        print('\n')
+        slow_print(f'{game.name} finds a special key. The key looks like a key for a storage room...')
+        countdown(3)
+        print('\n')
+        game.inventory += ['storage room key']
+        print('\n')
+        slow_print('Item: key for storage room picked up')
+        print('\n')
+        slow_print(f'{game.name} searched all chair rows.... ')
+        print('\n')
+        go_back()
+        
+    def go_back():
+        slow_print(f'{game.name} is back in the middle of the lecture hall now and ... ')
+        countdown(3)
+        leave_room()
+        
+    def relax():
+        print('\n')
+        slow_print(f'{game.name} sits down on the first chair and try to relax for some time. ')
+        print('\n')
+        countdown(10)
+        print('\n')
+        slow_print(f'What does {game.name} want to do now?')
+        choises_2 = ['search chair row 1', 'go back']
+        choise = player_input(choises_2, decision_beer=0)
+        if choise == 1:
+            chair_row_1()
+        if choise == 2:
+            go_back()
+        
+# Chair Rows
+    slow_print(f'''
+{game.name} is standing besides the student chair rows.
+What does {game.name} want to do now?''')
+    print('\n')
+    choises_2 = ['sit down and relax for some time', 'search chair row 1']
+    choise = player_input(choises_2, decision_beer=0)
+    if choise == 1:
+        relax()
+            
+    if choise == 2:
+        chair_row_1()
+        
 
-    slow_print(text_pattern_1)
-    player_choice = int(input('''
-    1. Umsehen nach Anhaltszeichen
-    2. Lichtschalter suchen
-    3. Raum verlassen
-    '''))
->>>>>>> main
+        
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        
+        
 
 if __name__ == "__main__":
     class Game:
@@ -246,5 +305,6 @@ if __name__ == "__main__":
             self.name = input(str('name:  ')).upper()
             self.visited_rooms = []
 
-    game = Game()
-    lecture_hall(game)
+game = Game()
+lecture_hall(game)
+        
